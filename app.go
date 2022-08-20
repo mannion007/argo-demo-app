30943fde3b790e51abcb249799648c964fe6aee0
James Mannion <james.mannion@lendable.co.uk>
2022-08-20 23:32:45 +0100

    go web server

--- app.go
+++ app.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Printf("Starting server at port 8080\n")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
 }
